8eb0ffb3fcbb7f50dfeff92976f5ebdb90a4135a
Roye <roye@Roye.local>
2021-08-23 15:08:52 +0800

    update num extraction logic

--- num_preprocessing.py
+++ num_preprocessing.py
@@ -6,11 +6,11 @@ class Num_Preprocessor():
 
     def extract_date(self,sent,nlp):
         doc = nlp(sent)
-        ents = [e.text for e in doc.ents if e.label_=='DATE' or e.label_=='TIME']
+        ents = [(e.text,e.start_char,e.start_char+len(e.text)) for e in doc.ents if e.label_=='DATE' or e.label_=='TIME']
         extraced_date_sent = sent
         for ent in ents:
-            extraced_date_sent = extraced_date_sent.replace(ent,'')
-        return extraced_date_sent
+            extraced_date_sent = extraced_date_sent.replace(ent[0],'')
+        return extraced_date_sent,ents
 
     def check_phrase(self,phrase,remove_tokens):
         result_phrase = phrase
@@ -27,15 +27,15 @@ class Num_Preprocessor():
                 result_phrase += word
         return result_phrase
 
-    def check_non_num_chinese(self,phrase,remove_phrases=['一些','多少','大多數','多',]):
+    def check_non_num_chinese(self,phrase,remove_phrases=['一些','多少','大多','數','大多數','多','一半','數百萬','數千萬','數以萬計','大量','部分','兩半'],num_unit = ['十','千','萬','億']):
         result_phrase = phrase
         for remove_phrase in remove_phrases:
             if result_phrase == remove_phrase:
                 return ''
         return result_phrase
 
-    def add_num_phrase_procedure(self,num_phrase, num_count, num_dict,non_needed_phrase = ['','一','1','one']):
-        if num_phrase != '' and num_phrase != '一' and num_phrase != '1' and num_phrase != 'one':
+    def add_num_phrase_procedure(self,num_phrase, num_count, num_dict,non_needed_phrase = ['','一','one']):
+        if num_phrase not in non_needed_phrase:
             num_dict[f'<NUM{num_count}>'] = num_phrase
             num_phrase = ''
             num_count += 1
@@ -43,7 +43,7 @@ class Num_Preprocessor():
         return num_phrase,num_count,num_dict,is_phrase
 
     def extract_num(self,sent, nlp, split_sign =' ', remove_tokens = ['元','%']):
-        extracted_sent = self.extract_date(sent,nlp)
+        extracted_sent,date_ents = self.extract_date(sent,nlp)
         doc = nlp(extracted_sent)
         num_count = 0
         is_phrase = False
@@ -75,12 +75,18 @@ class Num_Preprocessor():
             num_phrase = self.check_phrase(num_phrase, remove_tokens)
             num_phrase, num_count, num_dict, is_phrase = self.add_num_phrase_procedure(num_phrase, num_count, num_dict)
 
-        return num_dict
+        return num_dict,date_ents
 
-    def replace_num(self,sent,num_dict):
+    def replace_num(self,sent,num_dict,ents):
         result_sent = sent
+        for i in range(len(ents)-1,-1,-1):
+            ent_start = ents[i][1]
+            ent_end = ents[i][2]
+            result_sent = result_sent[:ent_start] + '<DATE>' + result_sent[ent_end:]
         for key in num_dict:
             result_sent = result_sent.replace(num_dict[key],key,1)
+        for ent in ents:
+            result_sent = result_sent.replace('<DATE>',ent[0],1)
         return result_sent
 
     def fill_num(self,sent,num_dict):
@@ -113,22 +119,28 @@ if __name__=='__main__':
         zh_fp = open(os.path.join(training_data_path, zh_file))
     
         for en_data, zh_data in zip(en_fp,zh_fp):
-            en_corpus.append(en_data.sentip())
-            zh_corpus.append(zh_data.sentip())
+            en_corpus.append(en_data.strip())
+            zh_corpus.append(zh_data.strip())
     df = pd.DataFrame({'english_sent':en_corpus,'chinese_sent':zh_corpus})
-    
+
     # initialize chinese and english spacy nlp model.
     zh_nlp = spacy.load('zh_core_web_trf')
     en_nlp = spacy.load('en_core_web_trf')
+
     num_preprocessor = Num_Preprocessor()
+
     sent = 'On May 27, 2019, it will release NTD 50 million (73%) and transfer it to the account of the same name in Taipei Fubon Bank for operational turnover.'
-    en_num_dict = num_preprocessor.extract_num(sent, en_nlp)
-    print(num_preprocessor.replace_num(sent, en_num_dict))
+    en_num_dict,en_date_ents = num_preprocessor.extract_num(sent, en_nlp)
+    num_preprocessor.show_tokens_pos(sent,en_nlp)
+    print(num_preprocessor.replace_num(sent, en_num_dict, en_date_ents))
     print(en_num_dict)
-    
+
+    '''
     sent = '2019年5月27日放款新台幣5千萬元佔73%，轉入台北富邦銀行同名賬戶，用於業務周轉。'
     doc = zh_nlp(sent)
     for token in doc:
+        print(token.idx)
+        print(dir(token))
         print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
               token.shape_, token.is_alpha, token.is_stop)
     print(sent)
@@ -137,36 +149,36 @@ if __name__=='__main__':
     print(numed_zh_sent)
     print(zh_num_dict)
     print(num_preprocessor.fill_num(numed_zh_sent, en_num_dict))
+    '''
+    numed_zh_sents = []
+    zh_num_dicts = []
     count = 0
     for en_sen,zh_sen in zip(df['english_sent'],df['chinese_sent']):
-        zh_num_dict = num_preprocessor.extract_num(zh_sen, zh_nlp, split_sign='')
-        en_num_dict = num_preprocessor.extract_num(en_sen,en_nlp,split_sign=' ')
-        numed_zh_sent = num_preprocessor.replace_num(zh_sen, zh_num_dict)
-        numed_en_sent = num_preprocessor.replace_num(en_sen,en_num_dict)
+        zh_num_dict, zh_date_ents = num_preprocessor.extract_num(zh_sen, zh_nlp, split_sign='')
+        en_num_dict, en_date_ents = num_preprocessor.extract_num(en_sen,en_nlp,split_sign=' ')
+        numed_zh_sent = num_preprocessor.replace_num(zh_sen, zh_num_dict,zh_date_ents)
+        numed_en_sent = num_preprocessor.replace_num(en_sen,en_num_dict,en_date_ents)
+        numed_zh_sents.append(numed_zh_sent)
+        zh_num_dicts.append(zh_num_dict)
+
+    df['numed_chinese_sent'] = numed_zh_sents
+    df['zh_num_dict'] = zh_num_dicts
+    df.to_csv('training_data/numed_training_data.csv',index=False)
+    '''
         if len(zh_num_dict)!=0 |len(en_num_dict)!=0:
-            doc = zh_nlp(zh_sen)
-            for token in doc:
-                print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-                      token.shape_, token.is_alpha, token.is_stop)
-            doc = en_nlp(en_sen)
-            for token in doc:
-                print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-                      token.shape_, token.is_alpha, token.is_stop)
-    
+            num_preprocessor.show_tokens_pos(zh_sen,zh_nlp)
+            print(zh_sen)
             print(zh_num_dict,en_num_dict)
             print(numed_zh_sent)
             print(numed_en_sent)
             count +=1
-            if count > 1:
+            if count > 30:
                 break
     
-    sent = '首爾房價一年上漲18%'
-    zh_num_dict = num_preprocessor.extract_num(sent, zh_nlp, split_sign='')
-    numed_zh_sent = num_preprocessor.replace_num(sent, zh_num_dict)
+    sent = '一名男子走進奈洛比市一家汽車展示場，拿出大量1000肯亞先令紙鈔（約9歐元），買下一台豪華賓士轎車，急著將9月30日後就一文不值的現金脫手。'
+    zh_num_dict, zh_date_ents = num_preprocessor.extract_num(sent, zh_nlp, split_sign='')
+    numed_zh_sent = num_preprocessor.replace_num(sent, zh_num_dict,zh_date_ents)
     doc = zh_nlp(sent)
-    for token in doc:
-        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-              token.shape_, token.is_alpha, token.is_stop)
-    for ent in doc.ents:
-        print(ent.text, ent.start_char, ent.end_char, ent.label_)
+    print(zh_num_dict,zh_date_ents)
     print(numed_zh_sent)
+'''
