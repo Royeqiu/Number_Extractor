f79fd9caabd56821f417da2aeb53947e2110947f
Roye <roye@Roye.local>
2021-09-03 14:00:05 +0800

    reorganize example code and copy remove_phrases into non_needed_phrase

--- num_preprocessing.py
+++ num_preprocessing.py
@@ -29,14 +29,17 @@ class Num_Preprocessor():
 
     def check_non_num_chinese(self,phrase,remove_phrases=['一些','多少','大多','數','大多數','多','多數','眾多',
                                                           '一半','數百萬','數千萬','數以萬計','大量','許','諸多','很多','幾年','若干','半數','幾','幾票',
-                                                          '部分','兩半','許多','同一','一點','一家','一大堆','大部分','約半','組數十','好幾部','著好幾','多年','一大','一起','整','整數'],num_unit = ['十','千','萬','億']):
+                                                          '部分','部份','兩半','許多','同一','一點','一家','一大堆','大部分','約半','組數十','好幾部','著好幾','多年','一大','一起','整','整數'],num_unit = ['十','千','萬','億']):
         result_phrase = phrase
         for remove_phrase in remove_phrases:
             if result_phrase == remove_phrase:
                 return ''
         return result_phrase
 
-    def add_num_phrase_procedure(self,num_phrase, num_count, num_dict,non_needed_phrase = ['','一','one']):
+
+    def add_num_phrase_procedure(self,num_phrase, num_count, num_dict,non_needed_phrase = ['','一','one','一些','多少','大多','數','大多數','多','多數','眾多',
+                                                          '一半','數百萬','數千萬','數以萬計','大量','許','諸多','很多','幾年','若干','半數','幾','幾票',
+                                                          '部分','部份','兩半','許多','同一','一點','一家','一大堆','大部分','約半','組數十','好幾部','著好幾','多年','一大','一起','整','整數']):
         if num_phrase not in non_needed_phrase:
             num_dict[f'<NUM{num_count}>'] = num_phrase
             num_phrase = ''
@@ -108,6 +111,40 @@ class Num_Preprocessor():
         ents=[(ent.text, ent.label_)for ent in doc.ents]
         print(ents)
 
+
+def english_example():
+    sent = '2019年5月27日放款新台幣5千萬元佔73%，轉入台北富邦銀行同名賬戶，用於業務周轉。'
+    doc = zh_nlp(sent)
+    for token in doc:
+        print(token.idx)
+        print(dir(token))
+        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+              token.shape_, token.is_alpha, token.is_stop)
+    print(sent)
+    zh_num_dict = num_preprocessor.extract_num(sent, zh_nlp, split_sign='')
+    numed_zh_sent = num_preprocessor.replace_num(sent, zh_num_dict)
+    print(numed_zh_sent)
+    print(zh_num_dict)
+    # print(num_preprocessor.fill_num(numed_zh_sent, en_num_dict))
+
+def chinese_example():
+    #那名失蹤近<NUM0>的小女孩，如今推斷應該已不在人世。
+    #該鎮所有人已被困在家一個月。
+    #所有離海岸不足100米的房子都有被洪水襲擊的危險。
+    #被「男性健康」雜誌形容為「平均每戶學位數量多過溫度計上的刻度」的這座美國中西部大都會，擠掉德州的普蘭諾與北卡羅萊納州的萊里，在該雜誌的人口教育程度最佳的全<NUM0>大城市排名中，拿下第一名。
+    #法國最近終止了Minitel服務，這種由法國自行發展的網路前身早在網際網路出現10年前，就為數 < NUM0 > 國民眾提供了線上金融、旅遊訂位，甚至性愛聊天室等服務。
+    #「以前一個客人上門，我們總是能夠賣<NUM0>、<NUM1>樣產品給他們。現在，唉，他們通常就只會買一樣。我們確實感受經濟不景氣帶來的衝擊。」他說。
+    sent = '當電力公司打開球中成千上萬的小燈時，人們恐中喊著“喔”和“啊”．'
+    #sent = '最大面額1000先令的新版紙鈔在六月推出，這項行動旨在迫使逃稅者、奸商和犯罪集團，釋出積存的來歷不明財富。肯亞央行六月表示，約有2.18億張千元先令在市面流通。'
+    zh_num_dict, zh_date_ents = num_preprocessor.extract_num(sent, zh_nlp, split_sign='')
+    numed_zh_sent = num_preprocessor.replace_num(sent, zh_num_dict,zh_date_ents)
+    doc = zh_nlp(sent)
+    print(sent)
+    num_preprocessor.show_tokens_pos(sent,zh_nlp)
+    print(zh_num_dict,zh_date_ents)
+    print(numed_zh_sent)
+
+
 if __name__=='__main__':
     # load long and short corpus
     training_data_path = 'training_data/'
@@ -130,28 +167,6 @@ if __name__=='__main__':
     en_nlp = spacy.load('en_core_web_trf')
 
     num_preprocessor = Num_Preprocessor()
-
-    sent = 'On May 27, 2019, it will release NTD 50 million (73%) and transfer it to the account of the same name in Taipei Fubon Bank for operational turnover.'
-    en_num_dict,en_date_ents = num_preprocessor.extract_num(sent, en_nlp)
-    num_preprocessor.show_tokens_pos(sent,en_nlp)
-    print(num_preprocessor.replace_num(sent, en_num_dict, en_date_ents))
-    print(en_num_dict)
-
-    '''
-    sent = '2019年5月27日放款新台幣5千萬元佔73%，轉入台北富邦銀行同名賬戶，用於業務周轉。'
-    doc = zh_nlp(sent)
-    for token in doc:
-        print(token.idx)
-        print(dir(token))
-        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-              token.shape_, token.is_alpha, token.is_stop)
-    print(sent)
-    zh_num_dict = num_preprocessor.extract_num(sent, zh_nlp, split_sign='')
-    numed_zh_sent = num_preprocessor.replace_num(sent, zh_num_dict)
-    print(numed_zh_sent)
-    print(zh_num_dict)
-    print(num_preprocessor.fill_num(numed_zh_sent, en_num_dict))
-    '''
     numed_zh_sents = []
     zh_num_dicts = []
     count = 0
@@ -171,28 +186,5 @@ if __name__=='__main__':
     df['zh_num_dict'] = zh_num_dicts
     df.to_csv('training_data/numed_training_data.csv',index=False)
 
-    '''
-        if len(zh_num_dict)!=0 |len(en_num_dict)!=0:
-            num_preprocessor.show_tokens_pos(zh_sen,zh_nlp)
-            print(zh_sen)
-            print(zh_num_dict,en_num_dict)
-            print(numed_zh_sent)
-            print(numed_en_sent)
-            count +=1
-            if count > 30:
-                break
-    '''
-    #該鎮所有人已被困在家一個月。
-    #所有離海岸不足100米的房子都有被洪水襲擊的危險。
-    #被「男性健康」雜誌形容為「平均每戶學位數量多過溫度計上的刻度」的這座美國中西部大都會，擠掉德州的普蘭諾與北卡羅萊納州的萊里，在該雜誌的人口教育程度最佳的全<NUM0>大城市排名中，拿下第一名。
-    #法國最近終止了Minitel服務，這種由法國自行發展的網路前身早在網際網路出現10年前，就為數 < NUM0 > 國民眾提供了線上金融、旅遊訂位，甚至性愛聊天室等服務。
-    #「以前一個客人上門，我們總是能夠賣<NUM0>、<NUM1>樣產品給他們。現在，唉，他們通常就只會買一樣。我們確實感受經濟不景氣帶來的衝擊。」他說。
-    sent = '雖然人力仍嫌單薄，金色黎明現正試圖把其主張傳播至下一代。'
-    #sent = '最大面額1000先令的新版紙鈔在六月推出，這項行動旨在迫使逃稅者、奸商和犯罪集團，釋出積存的來歷不明財富。肯亞央行六月表示，約有2.18億張千元先令在市面流通。'
-    zh_num_dict, zh_date_ents = num_preprocessor.extract_num(sent, zh_nlp, split_sign='')
-    numed_zh_sent = num_preprocessor.replace_num(sent, zh_num_dict,zh_date_ents)
-    doc = zh_nlp(sent)
-    print(sent)
-    num_preprocessor.show_tokens_pos(sent,zh_nlp)
-    print(zh_num_dict,zh_date_ents)
-    print(numed_zh_sent)
+
+
